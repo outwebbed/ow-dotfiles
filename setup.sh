6902e0b5c616dde525120f70ed8c647cd50d5070
Mike <webguy@outwebbed.com>
2022-07-15 12:53:28 +0000

    fix: add quotes to git config params

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo "setting git config..."
-git config --global user.name $GITHUB_USER
-git config --global user.email $GITHUB_EMAIL
+git config --global user.name "$GITHUB_USER"
+git config --global user.email "$GITHUB_EMAIL"
